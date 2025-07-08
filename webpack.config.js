f177159ee9213f3e2a0563f9cd566a2514346461
roymichaels <RoyMichaelsDotCom@pm.me>
2025-07-08 16:45:39 +0700

    kk

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,15 @@
-const { createWebpackConfigAsync } = require('@expo/webpack-config');
-const webpack = require('webpack');
+// webpack.config.js
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
-  const config = await createWebpackConfigAsync(env, argv);
+  const config = await createExpoWebpackConfigAsync(env, argv);
 
-  // 1) prepend our polyfills.js to the very first entry
-  const appEntry = config.entry.main;
-  config.entry.main = ['./polyfills.js', appEntry];
-
-  // 2) shim missing Node modules
-  config.resolve.fallback = {
-    crypto: require.resolve('crypto-browserify'),
-    stream: require.resolve('stream-browserify'),
-    assert: require.resolve('assert/'),
-    util: require.resolve('util/'),
-  };
-
-  // 3) auto‐provide Buffer & process
-  config.plugins.push(
-    new webpack.ProvidePlugin({
-      Buffer: ['buffer', 'Buffer'],
-      process: 'process/browser',
-    })
-  );
-
-  // keep your existing source‐map tweak
   if (config.mode === 'production') {
+    // remove Expo’s broken scope‐hoisting plugin in prod
+    config.plugins = config.plugins.filter(
+      (p) => p.constructor.name !== 'WebpackDeepScopeAnalysisPlugin'
+    );
+    // optionally keep source-maps:
     config.devtool = 'source-map';
   }
 
