6e9ac3794c236fed03feeea3601df6276ddfb70e
roymichaels <RoyMichaelsDotCom@pm.me>
2025-07-11 17:14:36 +0700

    fix web runtime require error

--- polyfills.js
+++ polyfills.js
@@ -2,8 +2,18 @@ import 'expo-standard-web-crypto';
 import 'react-native-get-random-values';
 import 'react-native-url-polyfill/auto';
 import { Buffer } from 'buffer';
+import * as SafeAreaContext from 'react-native-safe-area-context';
 
 if (typeof global.Buffer === 'undefined') {
   global.Buffer = Buffer;
+}
+
+if (typeof global.require === 'undefined') {
+  global.require = (moduleName) => {
+    if (moduleName === 'react-native-safe-area-context') {
+      return SafeAreaContext;
+    }
+    throw new Error(`Cannot find module '${moduleName}'`);
+  };
 }
 
