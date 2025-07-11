7c95346e4d546c7c32201e6e54b4ae978d5cfd97
roymichaels <RoyMichaelsDotCom@pm.me>
2025-07-11 22:35:51 +0700

    s

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
-    plugins: ['@babel/plugin-transform-modules-commonjs'],
+    // no plugins here
   };
 };
