2a183dfa5b5ab817c7ec4d1a805f7ae5d72e7443
roymichaels <RoyMichaelsDotCom@pm.me>
2025-07-11 12:40:14 +0700

    Add expo-router babel plugin

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,7 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'babel-plugin-transform-import-meta',
+      'expo-router/babel',
       'react-native-reanimated/plugin', // keep last
     ],
   };
