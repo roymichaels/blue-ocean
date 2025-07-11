8aa2d7021bc420c40d230443eeebc42d8693ec26
roymichaels <RoyMichaelsDotCom@pm.me>
2025-07-11 19:42:10 +0700

    ss

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,19 @@
-// webpack.config.js
-import createExpoWebpackConfigAsync from '@expo/webpack-config';
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
-export default async function (env, argv) {
+module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
 
-  // rely on Expo's default Node.js polyfills
-  // previously polyfills were disabled here but that caused issues with packages
-
-  if (config.mode === 'production') {
-    // remove Expo’s broken scope‐hoisting plugin in prod
-    config.plugins = config.plugins.filter(
-      (p) => p.constructor.name !== 'WebpackDeepScopeAnalysisPlugin'
-    );
-    config.devtool = 'source-map';
-  }
+  config.module.rules.unshift({
+    test: /\.js$/,
+    include: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['babel-preset-expo'],
+        plugins: ['@babel/plugin-transform-modules-commonjs'],
+      },
+    },
+  });
 
   return config;
-}
-
-config.module.rules.unshift({
-  test: /\.js$/,
-  include: /node_modules/,
-  use: {
-    loader: 'babel-loader',
-    options: {
-      plugins: ['@babel/plugin-transform-modules-commonjs'],
-    },
-  },
-});
+};
