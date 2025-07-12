e6f7ed46568850ef98a128c7f15d5ab5232acdee
roymichaels <RoyMichaelsDotCom@pm.me>
2025-07-12 13:37:12 +0700

    Simplify Babel config and refresh lockfile

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,4 @@
 module.exports = function (api) {
   api.cache(true);
-  return {
-    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
-    plugins: [
-      [
-        '@babel/plugin-transform-runtime',
-        {
-          helpers: true,
-          regenerator: true,
-          useESModules: true, // <-- keep it ES modules
-        },
-      ],
-    ],
-  };
+  return { presets: ['babel-preset-expo'] };
 };
