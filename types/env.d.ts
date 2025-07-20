e1763e45eafa03263e57100330ee61737cacfe1c
roymichaels <RoyMichaelsDotCom@pm.me>
2025-07-21 06:32:06 +0700

    chore(types): add env vars

--- types/env.d.ts
+++ types/env.d.ts
@@ -5,6 +5,15 @@ declare global {
       EXPO_PUBLIC_MATRIX_SERVER: string;
       EXPO_PUBLIC_APP_NAME: string;
       EXPO_PUBLIC_DEBUG_LOGS?: string;
+      EXPO_PUBLIC_ADMIN_PASSWORD?: string;
+      EXPO_PUBLIC_JWT_SECRET?: string;
+      EXPO_PUBLIC_PINATA_JWT?: string;
+      EXPO_PUBLIC_PINATA_API_KEY?: string;
+      EXPO_PUBLIC_PINATA_SECRET_API_KEY?: string;
+      EXPO_PUBLIC_CHAT_SECRET?: string;
+      VAPID_PUBLIC_KEY?: string;
+      VAPID_PRIVATE_KEY?: string;
+      EXPO_PUBLIC_TENANT?: string;
     }
   }
 }
