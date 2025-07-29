6b85aa1f0951a7140d7055ac5d9f14b908203a35
roymichaels <RoyMichaelsDotCom@pm.me>
2025-07-30 04:57:07 +0700

    Handle pending DB closure in getDatabase

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,11 @@ module.exports = {
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
     '\\.(png|jpg|jpeg|gif|svg)$': '<rootDir>/tests/__mocks__/fileMock.js',
+    '^expo-sqlite$': '<rootDir>/tests/__mocks__/expo-sqlite.js',
+    '^expo-file-system$': '<rootDir>/tests/__mocks__/expo-file-system.js',
+    '^expo-asset$': '<rootDir>/tests/__mocks__/expo-asset.js',
+    '^react-native$': '<rootDir>/tests/__mocks__/react-native.js',
   },
-  setupFiles: ['<rootDir>/tests/setupEnv.ts'],
+  setupFiles: ['<rootDir>/tests/initGlobals.ts'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setupEnv.ts'],
 };
