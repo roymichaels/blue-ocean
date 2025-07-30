0b0adc39c67a547b5c0e3fc9643e61d977039fb2
roymichaels <RoyMichaelsDotCom@pm.me>
2025-07-30 16:55:42 +0700

    chore: clean up sqlite artifacts

--- jest.config.js
+++ jest.config.js
@@ -6,12 +6,11 @@ module.exports = {
     '^.+\\.tsx?$': ['ts-jest', { useESM: true }],
     '^.+\\.js$': 'babel-jest',
   },
-  transformIgnorePatterns: ['/node_modules/(?!(@noble/ed25519|expo-sqlite|react-native)/)'],
+  transformIgnorePatterns: ['/node_modules/(?!(@noble/ed25519|react-native)/)'],
 
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
     '\\.(png|jpg|jpeg|gif|svg)$': '<rootDir>/tests/__mocks__/fileMock.js',
-    '^expo-sqlite$': '<rootDir>/tests/__mocks__/expo-sqlite.js',
     '^expo-file-system$': '<rootDir>/tests/__mocks__/expo-file-system.js',
     '^expo-asset$': '<rootDir>/tests/__mocks__/expo-asset.js',
     '^react-native$': '<rootDir>/tests/__mocks__/react-native.js',
