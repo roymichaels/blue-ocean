085f39c17875a80c83751770f67e27d16895bc04
roymichaels <RoyMichaelsDotCom@pm.me>
2025-09-03 17:38:30 -0500

    refactor category screen to use service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,7 @@
 export * from './useProducts';
 export * from './useStores';
 export * from './useCategories';
+export * from './useCategory';
 export * from './useCart';
 export * from './useOrders';
 export * from './useUser';
