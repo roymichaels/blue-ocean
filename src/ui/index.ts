c883444f3e8e35e9f9124cfb92e19ba7c379abbe
roymichaels <RoyMichaelsDotCom@pm.me>
2025-09-04 15:44:25 -0500

    chore: use UI barrel imports

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,3 +1,27 @@
-export * from './primitives';
-export * from './layout';
+export {
+  Avatar,
+  Badge,
+  Button,
+  Card,
+  Chip,
+  Divider,
+  Heading,
+  Menu,
+  type MenuItem,
+  Overlay,
+  Portal,
+  Skeleton,
+  Spinner,
+  Text,
+  TextField,
+} from './primitives';
+
+export {
+  Container,
+  Grid,
+  Screen,
+  ScrollArea,
+  Spacer,
+  Stack,
+} from './layout';
 
