f324509316b9e24cb399f0b0adefb9c0e84068fe
roymichaels <RoyMichaelsDotCom@pm.me>
2025-09-05 08:08:13 -0500

    refactor: simplify root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import { Slot } from 'expo-router';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AppProviders } from '@/providers';
 
 export default function RootLayout() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <SafeAreaProvider>
-        <AppProviders>
-          <Slot />
-        </AppProviders>
-      </SafeAreaProvider>
-    </GestureHandlerRootView>
+    <AppProviders>
+      <Slot />
+    </AppProviders>
   );
 }
