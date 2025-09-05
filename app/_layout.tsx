df6768ef9b431d06553c8d7d058a3ea57517612d
roymichaels <RoyMichaelsDotCom@pm.me>
2025-09-05 14:32:14 -0500

    feat: expose global t function

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Slot } from 'expo-router';
-import { AppProviders } from '@/providers';
+import AppProviders from '@/providers/AppProviders';
 
 export default function RootLayout() {
   return (
