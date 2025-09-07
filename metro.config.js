e2eaaccb9a8bc11611bca285c7079555c13cdb32
roymichaels <RoyMichaelsDotCom@pm.me>
2025-09-07 14:27:44 -0500

    fix: alias waku version shim for metro

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,24 @@
+const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
-if (!config.resolver.sourceExts.includes('mjs')) config.resolver.sourceExts.push('mjs');
-if (!config.resolver.sourceExts.includes('cjs')) config.resolver.sourceExts.push('cjs');
+
+config.resolver.extraNodeModules = {
+  ...(config.resolver.extraNodeModules || {}),
+  '@waku/core/lib/message/version_0':
+    path.resolve(
+      __dirname,
+      'node_modules/@waku/core/dist/lib/message/version_0.js'
+    ),
+  '@waku/core/lib/message/version-0':
+    path.resolve(
+      __dirname,
+      'node_modules/@waku/core/dist/lib/message/version_0.js'
+    ),
+};
+
+if (!config.resolver.sourceExts.includes('mjs'))
+  config.resolver.sourceExts.push('mjs');
+if (!config.resolver.sourceExts.includes('cjs'))
+  config.resolver.sourceExts.push('cjs');
+
 module.exports = config;
