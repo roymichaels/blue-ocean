d6ef5bab607c57fc1f89431dbdbfe4541c0f8841
roymichaels <RoyMichaelsDotCom@pm.me>
2025-09-08 08:52:54 -0500

    fix: render home screen instead of placeholder

--- app/landing.tsx
+++ app/landing.tsx
@@ -3,8 +3,10 @@ import { useAppRouter } from '@/services';
 
 export default function LandingRedirect() {
   const { replace } = useAppRouter();
+
   useEffect(() => {
     replace('/');
   }, [replace]);
+
   return null;
 }
